95b8605a23d519e167d619eb2084f69917d140d4
Arc-Seijin <dhrv.mukh@gmail.com>
2025-04-03 15:15:14 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 const bedrock = require('bedrock-protocol');
+const fs = require('fs');
 
 const SERVER_HOST = 'Test-LEaV.aternos.me';
 const SERVER_PORT = 31944;
@@ -6,8 +7,18 @@ const USERNAME_1 = 'chikabot69';
 const USERNAME_2 = 'ChikaBadmoosh';
 const AUTH_TYPE = 'microsoft';
 
-let bot1 = null;
-let bot2 = null;
+const TOKEN_FILE = 'auth_cache.json';
+
+function loadAuthCache() {
+    if (fs.existsSync(TOKEN_FILE)) {
+        return JSON.parse(fs.readFileSync(TOKEN_FILE, 'utf8'));
+    }
+    return {};
+}
+
+function saveAuthCache(data) {
+    fs.writeFileSync(TOKEN_FILE, JSON.stringify(data, null, 2), 'utf8');
+}
 
 function startBot(username, duration, onReady) {
     console.log(`[BOT] Connecting as ${username}...`);
@@ -17,6 +28,8 @@ function startBot(username, duration, onReady) {
         port: SERVER_PORT,
         username: username,
         auth: AUTH_TYPE,
+        profilesFolder: '.', // Enables saving Microsoft tokens
+        cacheDir: '.',       // Store tokens in the current directory
     });
 
     bot.on('login', () => console.log(`[BOT] ${username} logged in.`));
@@ -24,6 +37,11 @@ function startBot(username, duration, onReady) {
     bot.on('spawn', () => {
         console.log(`[BOT] ${username} spawned.`);
         if (onReady) onReady(bot);
+
+        // Save authentication token
+        if (bot.options.authflow && bot.options.authflow.cache) {
+            saveAuthCache(bot.options.authflow.cache);
+        }
 
         // Disconnect after the specified duration
         setTimeout(() => {
